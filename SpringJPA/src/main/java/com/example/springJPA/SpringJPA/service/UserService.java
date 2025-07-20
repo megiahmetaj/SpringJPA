ec26ae9d43bafe2264ce933f317101c2bae86b88
megiahmetaj <megiahmetaj2005@gmail.com>
2025-07-20 10:24:54 +0200

    changes

--- SpringJPA/src/main/java/com/example/springJPA/SpringJPA/service/UserService.java
+++ SpringJPA/src/main/java/com/example/springJPA/SpringJPA/service/UserService.java
@@ -1,10 +1,12 @@
 package com.example.springJPA.SpringJPA.service;
 
+import com.example.springJPA.SpringJPA.dto.UserDTO;
 import com.example.springJPA.SpringJPA.model.User;
 import com.example.springJPA.SpringJPA.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -19,6 +21,17 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    // metoda qe kerkon sipas emrit
+    // metoda qe kerkon sipas emrit dhe kthen nje objekt userDTO
+    public List<UserDTO> searchByName(String name){
+        return userRepository.findByName(name)
+                .stream().map(user -> new UserDTO(user.getName(), user.getEmail()))
+                .collect(Collectors.toList());
+    }
+
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+
 
 }
